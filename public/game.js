2298cc94e699df51a7cb9dcda34b37a4aaf27c8a
Alexandar Karapenev <treildr@gmail.com>
2025-07-29 16:53:12 +0200

    Added raycaster

--- public/game.js
+++ public/game.js
@@ -13,7 +13,16 @@ const config = {
         preload,
         create,
         update
-    }
+    },
+    plugins: {
+    scene: [
+      {
+        key: 'raycasterPlugin',
+        plugin: PhaserRaycaster,
+        mapping: 'raycasterPlugin'
+      }
+    ]
+  }
 };
 
 const TILE_SIZE = 90;
@@ -47,9 +56,14 @@ let heldRight = false;
 let heldUp = false;
 let heldDown = false;
 
-function preload() {
-    // Light gradeint
+let visibleBlocks;
+let blocksMap = {}; 
 
+function preload() {
+
+    this.load.image('block', 'https://labs.phaser.io/assets/sprites/block.png');
+
+    // Light gradeint
     const size = TILE_SIZE * 7;
     const rt = this.textures.createCanvas('lightGradient', size, size);
     const ctx = rt.getContext();
@@ -120,10 +134,25 @@ function create() {
     })
     .setScrollFactor(0)
     .setDepth(100);
+
+    // Visible blocks from raycaster
+    visibleBlocks = this.physics.add.staticGroup();
+    this.renderedCells = new Set();
+
+    // TODO fix collider
+    //this.physics.add.collider(this.player, this.visibleBlocks);
+    this.cursors = this.input.keyboard.createCursorKeys();
+
+    // Raycaster
+    this.raycaster = this.raycasterPlugin.createRaycaster();
+    //this.raycaster.setBoundingBox(0, 0, this.worldCols * this.gridSize, this.worldRows * this.gridSize);
+    this.ray = this.raycaster.createRay({ origin: this.player, autoSlice: true });
+
+    this.lightGraphics = this.add.graphics({ fillStyle: { color: 0xffffaa, alpha: 0.3 } });
 }
 
 function update() {
-    // Light
+    // Light gradeint
     this.darkness.clear();
     this.darkness.fill(0x000000, 1);
 
@@ -138,6 +167,9 @@ function update() {
     const fps = Math.floor(this.game.loop.actualFps);
     this.fpsText.setText(`FPS: ${fps}`);
 
+    // Update the raycaster
+    updateRaycaster.call(this);
+
     // Movement
     if (moving) return;
 
@@ -146,6 +178,78 @@ function update() {
     if (heldUp && tryMove(0, -1)) return;
     if (heldDown && tryMove(0, 1)) return;
 }
+
+function updateVisibleBlocks() {
+    const radius = 10;
+
+    // if its called before initiated
+    if(!visibleBlocks)
+        return;
+
+    const startX = Math.max(0, player.gridX - radius);
+    const endX = Math.min(GRID_WIDTH, player.gridX + radius + 1);
+    const startY = Math.max(0, player.gridY - radius);
+    const endY = Math.min(GRID_HEIGHT, player.gridY + radius + 1);
+
+    // Add Visible
+    for (let y = startY; y < endY; y++) {
+        for (let x = startX; x < endX; x++) {
+
+            if(grid[y][x] == 1){
+
+                const key = `${x}_${y}`;
+
+                if (!blocksMap[key]) {
+                    const block = visibleBlocks.create(x * TILE_SIZE, y * TILE_SIZE, 'block')
+                        .setOrigin(0)
+                        .setDisplaySize(TILE_SIZE, TILE_SIZE);
+                    block.refreshBody();
+                    blocksMap[key] = block;
+                }
+            }
+        }
+    }
+
+    for (const key in blocksMap) {
+        const [bx, by] = key.split('_').map(Number);
+        if (bx < startX || bx >= endX || by < startY || by >= endY) {
+            this.raycaster.removeMappedObjects(blocksMap[key]);
+            blocksMap[key].destroy();
+            delete blocksMap[key];
+        }
+    }
+}
+
+function updateRaycaster() {
+    const cam = this.cameras.main;
+    const gridSize = TILE_SIZE;
+    const buffer = 10; // buffer zone
+
+    const left = Math.floor(cam.worldView.x / gridSize) - buffer;
+    const right = Math.ceil((cam.worldView.x + cam.width) / gridSize) + buffer;
+    const top = Math.floor(cam.worldView.y / gridSize) - buffer;
+    const bottom = Math.ceil((cam.worldView.y + cam.height) / gridSize) + buffer;
+
+    const offset = 60; // debug for bounds
+    const x = left * gridSize;
+    const y = top * gridSize;
+    const width = (right - left) * gridSize;
+    const height = (bottom - top) * gridSize;
+
+    this.raycaster.mapGameObjects(visibleBlocks.getChildren(), true);
+    this.ray.setOrigin(player.x, player.y);
+    const intersections = this.ray.castCircle();
+
+    this.lightGraphics.clear();
+    if (intersections.length > 0) {
+      this.lightGraphics.fillPoints(intersections, true);
+    }
+
+    this.raycaster.setBoundingBox(x + offset, y + offset, width - offset * 2, height - offset * 2);
+
+    // console.log(x, y, width, height); // debug raycaster visible cam
+}
+
 
 function drawVisibleTiles() {
     const radius = 10;
@@ -166,6 +270,8 @@ function drawVisibleTiles() {
             mapGraphics.fillRect(posX, posY, TILE_SIZE - 2, TILE_SIZE - 2);
         }
     }
+
+    updateVisibleBlocks.call(this);
 }
 
 
